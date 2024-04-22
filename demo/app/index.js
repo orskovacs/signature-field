ed386602160902d58f539089d5965c207e1b4143
Örs Kovács <46872866+orskovacs@users.noreply.github.com>
2024-04-22 18:16:08 +0200

    Skeleton for the demo app

--- demo/app/index.js
+++ demo/app/index.js
@@ -2,8 +2,15 @@ import '../../../../../../../dist/src/signature-field.js';
 
 import '@material/web/button/filled-button.js';
 import '@material/web/button/outlined-button.js';
+import '@material/web/button/text-button.js';
+import '@material/web/button/filled-tonal-button.js';
 import '@material/web/checkbox/checkbox.js';
 import '@material/web/radio/radio.js';
+import '@material/web/dialog/dialog.js';
+import '@material/web/list/list.js';
+import '@material/web/list/list-item.js';
+import '@material/web/tabs/primary-tab.js';
+
 import { styles as typescaleStyles } from '@material/web/typography/md-typescale-styles.js';
 import {
   argbFromHex,
@@ -11,16 +18,27 @@ import {
   applyTheme,
 } from '@material/material-color-utilities';
 
-if (typescaleStyles.styleSheet)
-  document.adoptedStyleSheets.push(typescaleStyles.styleSheet);
+function applyCustomTheme() {
+  if (typescaleStyles.styleSheet)
+    document.adoptedStyleSheets.push(typescaleStyles.styleSheet);
 
-const theme = themeFromSourceColor(argbFromHex('#f82506'), [
-  {
-    name: 'custom-1',
-    value: argbFromHex('#ff0000'),
-    blend: true,
-  },
-]);
-console.log(JSON.stringify(theme, null, 2));
-const systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-applyTheme(theme, { target: document.body, dark: systemDark });
+  const theme = themeFromSourceColor(argbFromHex('#f82506'), [
+    {
+      name: 'custom-1',
+      value: argbFromHex('#ff0000'),
+      blend: true,
+    },
+  ]);
+
+  const systemDarkMedia = window.matchMedia('(prefers-color-scheme: dark)');
+
+  applyTheme(theme, { target: document.body, dark: systemDarkMedia.matches });
+
+  systemDarkMedia.addEventListener('change', () => {
+    applyTheme(theme, { target: document.body, dark: systemDarkMedia.matches });
+  });
+}
+
+window.addEventListener('load', () => {
+  applyCustomTheme();
+});
