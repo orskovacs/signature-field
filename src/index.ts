9ebc435ef4c9dcdd4ac7cf2aa2a285474b816614
Örs Kovács <46872866+orskovacs@users.noreply.github.com>
2024-05-01 22:13:40 +0200

    Export types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export { SignatureField } from './elements/signature-field/SignatureField.js';
+export { Signature } from './models/signature.js';
+export type { SignatureDataPoint } from './models/signature-data-point.js';
